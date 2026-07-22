6365db82cee192cb2852938c33427e350aa0df0b
apurbo-singha01 <apurbosingha497@gmail.com>
2026-07-22 23:28:55 +0530

    Updated listing schema and data

--- models/listing.js
+++ models/listing.js
@@ -2,24 +2,24 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title : {
-        type : String,
-        required : true,
-    },
-    description : String,
-    image : {
-        type : String,
-        default : "https://unsplash.com/photos/alpine-village-on-lake-with-church-and-mountains--tgfpopqIV4", // if link is undefined then it is used
-        set : (v) =>  // if link is empty
-             v === "" 
-          ?"https://unsplash.com/photos/alpine-village-on-lake-with-church-and-mountains--tgfpopqIV4"
-           : v,
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        : v,
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
 
-    },
-    price : Number,
-    location : String,
-    country : String,
-})
-
-const Listing = mongoose.model("Listing",listingSchema);
+const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
